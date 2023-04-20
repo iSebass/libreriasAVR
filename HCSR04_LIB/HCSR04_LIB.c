1341839ac21ee1e38ba098905a62b55b4665db0e
iSebass <jusecofe@gmail.com>
2023-04-20 08:42:00 -0500

    Se agrega nueva libreria HCSR04 al repositorio

---
+++ HCSR04_LIB/HCSR04_LIB.c
@@ -0,0 +1,21 @@
+﻿#include "HCSR04_LIB.h"
+
+void HCSR04_Init(void){
+	DDR_HCSR04  |=  (1<<PIN_TRI);
+	DDR_HCSR04  &= ~(1<<PIN_ECHO);
+	PORT_HCSR04 &= ~(1<<PIN_TRI);
+}
+uint16_t HCSR04_get_distance(void){
+	uint16_t distance=0;
+	PORT_HCSR04 |=  (1<<PIN_TRI);
+	_delay_us(10);
+	PORT_HCSR04 &= ~(1<<PIN_TRI);
+	
+	while( getStatusECHO() == 0 );
+	while( getStatusECHO() != 0 ){
+		distance++;
+		_delay_us(58);
+	}
+	return (distance);
+	
+}
---
+++ HCSR04_LIB/HCSR04_LIB.h
@@ -0,0 +1,32 @@
+﻿/*
+ * HCSR04.h
+ *
+ * Created: 20/04/2023 7:38:03 a. m.
+ *  Author: iProf
+ */ 
+
+
+#ifndef HCSR04_LIB_H_
+#define HCSR04_LIB_H_
+
+#define F_CPU 16000000UL
+#include <stdint.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DDR_HCSR04			DDRB
+#define PIN_HCSR04			PINB
+#define PORT_HCSR04			PORTB
+#define PIN_ECHO			1
+#define PIN_TRI				0
+#define getStatusECHO()     (PIN_HCSR04 & (1<<PIN_ECHO))		
+
+#define ERROR_TIME_OUT     0
+#define SUCCES_PROCESS     1
+
+void HCSR04_Init(void);
+uint16_t HCSR04_get_distance(void);
+
+
+
+#endif /* HCSR04_H_ */
